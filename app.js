86ff9a37151c4ef605a373b1e29a44dc9dd1f94d
Sandrine Gautier <sgautier75017@gmail.com>
2018-06-07 13:16:03 +0200

    make it better second part

--- app.js
+++ app.js
@@ -92,7 +92,7 @@ app.use('/', routes);
 app.use('/users', users);
 
 //// Port ////
-app.set('port', (process.env.PORT || 3000));
+app.set('port', (process.env.PORT || 5000));
 
 app.listen(app.get('port'), function(){
 	console.log('Server started on port '+app.get('port'));
